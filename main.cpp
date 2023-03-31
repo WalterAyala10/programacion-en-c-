abbe8dcca3625da1227a38cbea2dccd6483ccde1
Franco <francocoronel2023@gmail.com>
2023-03-31 20:27:46 -0300

    trolazo

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ int main()
     int ** punterodoble;
     int  *psimple;
     char letra = -1;
-<<<<<<< HEAD
+
     printf("hola puton\");
-=======
+
     printf("El negro se la come\");
-    printf("hola que wea\");
->>>>>>> 68fe897483cd97130820f5d55972c749f8586144
+    printf("hola como estas trolazo\");
+
     return 0;
 }
 void hola()
