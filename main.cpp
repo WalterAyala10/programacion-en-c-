f13f313d8412d7de3df17fbb6d62be1c8a1dc2cc
WalterAyala10 <ayalawalter10@yahoo.com.ar>
2023-03-31 17:39:53 -0300

    version1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <conio.h>
+#include <string.h>
+#define dato_generico template <class TIPOD>
+using namespace std;
+int main()
+{
+    int ** punterodoble;
+    int  *psimple;
+    char letra = -1;
+    printf("hola puto \");
+    return 0;
+}
